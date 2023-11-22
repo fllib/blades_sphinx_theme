704a794f6b2a1dea0dc9f74dc66da231c4669c63
ShenghuiLi <lishenghui.uu@gmail.com>
2023-11-22 14:12:14 +0100

    Update

--- blades_sphinx_theme/extension/blades_sphinx.py
+++ blades_sphinx_theme/extension/blades_sphinx.py
@@ -128,7 +128,7 @@ def monkey_patch_find_autosummary_in_lines(
 
 
 def setup(app):
-    pyg.setup(app)
+    # pyg.setup(app)
     # Monkey-patch `sphinx.ext.autosummary.find_autosummary_in_lines`:
     autosummary.find_autosummary_in_lines = (
         monkey_patch_find_autosummary_in_lines)
